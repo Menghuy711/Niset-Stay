4b1976a47dc60992ebb288f49a6b28e143ec103f
Menghuy Lor <lormenghuy12@gmail.com>
2026-08-14 10:43:26 +0700

    Fix routing on GitHub Pages (set router basename)

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <AuthProvider>
       <App />
     </AuthProvider>
